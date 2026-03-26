7337d28bd6ece5d593310d78998c0e2bae40054a
Dung Ha <dung.ha@vucar.net>
2026-03-26 15:15:39 +0700

    chore: remove temporary check script

--- tmp_check_vals.ts
+++
@@ -1,9 +0,0 @@
-
-import { vucarV2Query } from "./lib/db"
-
-async function check() {
-  const result = await vucarV2Query("SELECT DISTINCT qualified FROM sale_status WHERE qualified IS NOT NULL", [])
-  console.log(result.rows)
-}
-
-check()
